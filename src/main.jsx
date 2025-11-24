f90a086f6c0bb17549fec2fd200551c9f9fac1c5
nothing937 <jayden2006lee@gmail.com>
2025-11-25 00:21:07 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // BrowserRouter for Cloudflare Pages
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
-)
+  </BrowserRouter>
+);
